e1c5004a7fd7bcf26480181ca13ddc90bf2d0425
ErrorDeleted <maldach1@student.cvtc.edu>
2023-02-19 13:43:18 -0600

    Jasmine Ran

--- server.js
+++ server.js
@@ -8,18 +8,38 @@ const newFolderName = function (folders) {
     if (sortedFolders[i] === `New Folder (${newFoldersNum})`) { newFoldersNum += 1 }
   }
 
-  console.log('**********')
-  console.log(`User has: ${folders}`)
-  console.log(`User folders sorted: ${sortedFolders}`)
-  console.log('New folder will be: ')
+  // console.log('**********')
+  // console.log(`User has: ${folders}`)
+  // console.log(`User folders sorted: ${sortedFolders}`)
+  // console.log('New folder will be: ')
   if (starterFolder === false) {
-    console.log('New Folder')
+    // console.log('New Folder')
+    if (folders.length === 0) {
+      folders = ['New Folder']
+      // console.log(folders)
+    } else {
+      folders.push('New Folder')
+      // console.log('New Folder')
+    }
   } else {
-    console.log(`New Folder (${newFoldersNum})`)
+    // console.log(`New Folder (${newFoldersNum})`)
+    folders.push(`New Folder (${newFoldersNum})`)
   }
-  console.log('\n')
+  // console.log('\n')
+  console.log(folders)
 }
-newFolderName(['New Folder (7)', 'New Folder', 'New Folder (5)', 'New Folder (9)'])
-newFolderName(['New Folder', 'New Folder (2)', 'New Folder (7)', 'New Folder (3)', 'New Folder (5)', 'New Folder (4)'])
-
+// newFolderName(['New Folder (7)', 'New Folder', 'New Folder (5)', 'New Folder (9)'])
+// newFolderName(['New Folder', 'New Folder (2)', 'New Folder (7)', 'New Folder (3)', 'New Folder (5)', 'New Folder (4)'])
+newFolderName([])
+newFolderName(['a folder', 'new folder'])
+newFolderName(['New Folder (1)', 'New Folder (2)', 'New Folder (3)'])
+newFolderName(['Documents', 'New Folder (5)', 'New Folder'])
+newFolderName(['New Folder (2)', 'New Folder'])
+newFolderName([
+  'New Folder (4)',
+  'New Folder (2)',
+  'New Folder',
+  'New Folder (3)',
+  'New Folder (6)'
+])
 module.exports = { newFolderName }
