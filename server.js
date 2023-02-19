21946863a08d97eedd81fcd65d4231f2fa6c2138
ErrorDeleted <maldach1@student.cvtc.edu>
2023-02-19 11:32:19 -0600

    Initial newFolderName function

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
+// Future intent is to make this work with folders over 9 (since sort is inaccoret.)
 const newFolderName = function (folders) {
-
+  let sortedFolders = folders.sort();
+  let starterFolder = false;
+  let newFoldersNum = 2;
+  for(let i = 0; i < folders.length; i++) {
+    if(sortedFolders[i] == 'New Folder') {starterFolder = true}
+    if(sortedFolders[i] == `New Folder (${newFoldersNum})`) {newFoldersNum += 1}
+  }
+  
+  console.log("**********")
+  console.log(`User has: ${folders}`)
+  console.log(`User folders sorted: ${sortedFolders}`)
+  console.log("New folder will be: ")
+  if(starterFolder == false) {
+    console.log("New Folder")
+  } else {
+    console.log(`New Folder (${newFoldersNum})`)
+  }
+  console.log("\n")
+  
 }
+newFolderName(["New Folder (7)", "New Folder", "New Folder (5)", "New Folder (9)"])
+
+newFolderName(["New Folder", "New Folder (2)", "New Folder (7)", "New Folder (3)", "New Folder (5)", "New Folder (4)"])
